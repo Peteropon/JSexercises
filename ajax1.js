e5e5cf5d561111a440c768f178e23449d51f87e6
Petros Tsavdaridis <peteropon@gmail.com>
2019-03-27 21:52:48 +0100

    Add cards.html and add functions for presenting data in ajax1

--- ajax1.js
+++ ajax1.js
@@ -1,18 +1,65 @@
-var httpRequest = new XMLHttpRequest;
-var products;
-httpRequest.onreadystatechange = function () {
-  if(httpRequest.readyState == 4) {
-    if (httpRequest.status == 200) {
-      console.log(httpRequest.response);
-     products = httpRequest.response;
+var xhr = new XMLHttpRequest;
+var data;
+var url = 'https://www.hulabeck.se/html/temp/response.json';
 
+var header = document.querySelector('header');
+var section = document.querySelector('section');
+
+xhr.onreadystatechange = function () {
+    if(xhr.readyState == 4) {
+        if (xhr.status == 200) {
+            console.log(xhr.response);
+            data = xhr.response;
+
+        } else {
+            console.log("Status is " + xhr.status);
+        }
     } else {
-      console.log("Status is " + httpRequest.status);
+        console.log(xhr.readyState);
     }
-  } else {
-    console.log(httpRequest.readyState);
-  }
+
+
+};
+xhr.open('GET', url, true);
+xhr.responseType='json';
+xhr.send();
+
+xhr.onload = function () {
+  console.log("Response successfully returned.");
+  showProducts(data);
+};
+
+
+function showProducts(jsonObj) {
+    var products = jsonObj['products'];
+
+    for (var i = 0; i < products.length; i++) {
+        var myArticle = document.createElement('article');
+        var myH2 = document.createElement('h2');
+        var myPar1 = document.createElement('p');
+        var myPar2 = document.createElement('p');
+        var myPar3 = document.createElement('p');
+        var myPar4 = document.createElement('p');
+        var myPar5 = document.createElement('p');
+        var myList = document.createElement('ul');
+    }
+
+    myH2.textContent = products[i].name;
+    myPar1.textContent = "Description: " + products[i].description;
+    myPar2.textContent = "Price: " + products[i].consumerPrice;
+    myPar3.textContent = "In stock: " + products[i].inStock;
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPar1);
+    myArticle.appendChild(myPar2);
+    myArticle.appendChild(myPar3);
+
+    section.appendChild(myArticle);
 }
-httpRequest.open('GET', 'https://www.hulabeck.se/html/temp/products.json', true);
-httpRequest.responseType='json';
-httpRequest.send();
+
+
+// function populateHeader(jsonObj) {
+//     var myH1 = document.createElement('h1');
+//     myH1.textContent = jsonObj.getLocalName;
+//     header.appendChild(myH1);
+// }
